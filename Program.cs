28bb648dea94ab31d945bb5884c1d38e6702b37d
Sinan Nar <Sinan.Nar@eroad.com>
2024-09-20 11:49:09 +1200

    initial parallel.invoke

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Parallel.Invoke(
+//     () => { Console.WriteLine("Hello"); },
+//     () => { Console.WriteLine("World"); });
+ParallelInvoke(
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("World"); });
+
+Console.WriteLine("done!");
+
+static void ParallelInvoke(params Action[] actions)
+{
+    var tasks = from action in actions select Task.Run(action);
+    Task.WaitAll(tasks.ToArray());
+}

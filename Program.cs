b6b628cd777cb30a0b4357bb9acec4986dffc679
Sinan Nar <Sinan.Nar@eroad.com>
2024-09-20 11:52:31 +1200

    parallelforeach implemented

--- Program.cs
+++ Program.cs
@@ -4,12 +4,38 @@
 //     () => { Console.WriteLine("World"); });
 ParallelInvoke(
     () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
+    () => { Console.WriteLine("Hello"); },
     () => { Console.WriteLine("World"); });
 
 Console.WriteLine("done!");
 
 static void ParallelInvoke(params Action[] actions)
 {
-    var tasks = from action in actions select Task.Run(action);
+    if (actions.Length < 10)
+    {
+        var tasks = from action in actions select Task.Run(action);
+        Task.WaitAll(tasks.ToArray());
+    }
+    else
+    {
+        ParallelForEach(actions, action => action());
+    }
+}
+
+static void ParallelForEach<T>(IEnumerable<T> source, Action<T> action)
+{
+    var tasks = from item in source select Task.Run(() => action(item));
     Task.WaitAll(tasks.ToArray());
 }
